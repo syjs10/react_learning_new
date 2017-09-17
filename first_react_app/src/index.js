e4607fbd7f3624a1038349aa2d0e14793c4f4793
syjs10 <350172898@qq.com>
2017-09-17 10:07:24 +0800

    chapter 01

---
+++ first_react_app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ClickCounter from './ClickCounter.js';
+// import './index.css';
+
+
+ReactDOM.render(
+    <ClickCounter />,
+    document.getElementById('root')
+);
